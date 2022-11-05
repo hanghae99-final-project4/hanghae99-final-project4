0234b14d3ced4edbdfa40e2d6d90f50b9168c4fc
Sanghyun Lee <shlee1027@daum.net>
2022-11-06 01:56:00 +0900

    Co-authored-by jaewoo, sanghyun

--- hanghae99final/src/shared/Router.js
+++ hanghae99final/src/shared/Router.js
@@ -1,12 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import MainPage from "../pages/MainPage";
+import StandPage from "../pages/StandPage";
+import SitdownPage from "../pages/SitdownPage";
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainPage />} />
+        <Route path="/sitdownpage" element={<SitdownPage />} />
+        <Route path="/standpage" element={<StandPage />} />
       </Routes>
     </BrowserRouter>
   );
